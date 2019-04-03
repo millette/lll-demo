1a14dc51344ac955510d532ef246a30f069581e6
Robin Millette <robin@millette.info>
2019-04-03 16:20:19 -0400

    Clean up main js.

--- main.js
+++ main.js
@@ -1,20 +1,7 @@
-// taken from https://bl.ocks.org/mbostock/1095795
-
 // npm
-import { deburr, uniq } from "lodash-es"
 import riot from "riot"
 
 // self
 import "./tags/index.js"
 
-/*
-riot.mixin({
-  fetchFollows,
-  fetchOne,
-  clearGraph,
-  deburr,
-  uniq,
-})
-*/
-
-const elApp = riot.mount("app")[0]
+riot.mount("app")
